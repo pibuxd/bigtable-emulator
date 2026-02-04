267c44d998c4f5a60b7106a2aad632bdfa62073c
styczynski <piotr@styczynski.in>
2026-02-04 04:05:21 +0100

    wip: add fmtlog

--- persist/logging.h
+++ persist/logging.h
@@ -1,10 +1,18 @@
 #ifndef GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_BIGTABLE_EMULATOR_PERSIST_LOGGING_H
 #define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_BIGTABLE_EMULATOR_PERSIST_LOGGING_H
 
-#define DBG(TEXT)                \
+#include "fmtlog.h"
+
+#define XNDBG(TEXT)                \
   if (true) {                    \
     std::cout << (TEXT) << "\n"; \
     std::cout.flush();           \
+  }
+
+#define DBG(...)                \
+  if (true) {                   \
+    logi(__VA_ARGS__);          \
+    fmtlog::poll();             \
   }
 
 #endif  // GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_BIGTABLE_EMULATOR_PERSIST_LOGGING_H
